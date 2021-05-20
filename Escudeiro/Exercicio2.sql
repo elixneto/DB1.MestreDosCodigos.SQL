7dd08b52c0e96691e341a1fa905402b9b20425c5
Élix Neto <elixneto@gmail.com>
2021-05-20 19:38:10 -0400

    Update Exercicio2.sql

--- Escudeiro/Exercicio2.sql
+++ Escudeiro/Exercicio2.sql
@@ -1,18 +1,29 @@
 USE mestredoscodigos;
 
--- DROP TABLE clinica;
+DROP TABLE IF EXISTS atendimento;
+DROP TABLE IF EXISTS historico_clinica_profissional;
+DROP TABLE IF EXISTS clinica_profissional;
+DROP TABLE IF EXISTS profissional;
+DROP TABLE IF EXISTS tipo_profissional;
+DROP TABLE IF EXISTS paciente;
+DROP TABLE IF EXISTS clinica;
+DROP TABLE IF EXISTS usuario_endereco;
+DROP TABLE IF EXISTS usuario;
+DROP TABLE IF EXISTS endereco;
+
 CREATE TABLE clinica (
 	id INT IDENTITY PRIMARY KEY,
-	cnpj VARCHAR(14) NOT NULL
+	cnpj VARCHAR(14) NOT NULL,
+	nome_fantasia VARCHAR(100) NOT NULL
 );
 
--- DROP TABLE tipo_profissional;
+
 CREATE TABLE tipo_profissional (
 	id INT IDENTITY PRIMARY KEY,
 	descricao VARCHAR(100) NOT NULL
 );
 
--- DROP TABLE profissional;
+
 CREATE TABLE profissional (
 	id INT IDENTITY PRIMARY KEY,
 	id_tipo_profissional INT NOT NULL,
@@ -23,7 +34,7 @@ CREATE TABLE profissional (
 		ON UPDATE CASCADE
 );
 
--- DROP TABLE clinica_profissional;
+
 CREATE TABLE clinica_profissional (
 	id_clinica INT NOT NULL,
 	id_profissional INT NOT NULL,
@@ -39,7 +50,7 @@ CREATE TABLE clinica_profissional (
 	PRIMARY KEY (id_clinica, id_profissional)
 );
 
--- DROP TABLE historico_clinica_profissional;
+
 CREATE TABLE historico_clinica_profissional (
 	uuid UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
 	id_clinica INT NOT NULL,
@@ -50,20 +61,33 @@ CREATE TABLE historico_clinica_profissional (
 		ON DELETE NO ACTION
 		ON UPDATE CASCADE
 );
+CREATE INDEX idx_historico_clinica_profissional_clinica ON historico_clinica_profissional (id_clinica);
+CREATE INDEX idx_historico_clinica_profissional_profissional ON historico_clinica_profissional (id_profissional);
+CREATE INDEX idx_historico_clinica_profissional_data_historico ON historico_clinica_profissional (data_historico DESC);
 
--- DROP TABLE paciente;
+
 CREATE TABLE paciente (
 	id INT IDENTITY PRIMARY KEY,
 	nome VARCHAR(100) NOT NULL,
 	data_nascimento DATE
 );
 
--- DROP TABLE atendimento;
+
+CREATE TABLE usuario (
+	id INT IDENTITY PRIMARY KEY,
+	nome VARCHAR(100) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	hash_senha VARCHAR(500) NOT NULL
+);
+CREATE INDEX idx_usuario_email ON usuario (email);
+
+
 CREATE TABLE atendimento (
 	id INT IDENTITY PRIMARY KEY,
 	id_paciente INT NOT NULL,
 	id_clinica INT NOT NULL,
 	id_profissional INT NOT NULL,
+	id_usuario_inclusao INT NOT NULL,
 	data_atendimento DATE NOT NULL,
 	aprovado BIT NOT NULL,
 	
@@ -73,13 +97,39 @@ CREATE TABLE atendimento (
 
 	FOREIGN KEY (id_clinica, id_profissional) REFERENCES clinica_profissional (id_clinica, id_profissional)
 		ON DELETE NO ACTION
+		ON UPDATE CASCADE,
+		
+	FOREIGN KEY (id_usuario_inclusao) REFERENCES usuario (id)
+		ON DELETE NO ACTION
 		ON UPDATE CASCADE
 );
+CREATE INDEX idx_atendimento_data_atendimento ON atendimento (data_atendimento DESC);
+CREATE INDEX idx_atendimento_paciente ON atendimento (id_paciente);
 
--- DROP TABLE usuario;
-CREATE TABLE usuario (
+
+CREATE TABLE endereco (
 	id INT IDENTITY PRIMARY KEY,
-	nome VARCHAR(100) NOT NULL,
-	email VARCHAR(100) NOT NULL,
-	hash_senha VARCHAR(500) NOT NULL
+	rua VARCHAR(100) NOT NULL,
+	bairro VARCHAR(100) NOT NULL,
+	cep VARCHAR(8)
 );
+CREATE INDEX idx_endereco_cep ON endereco (cep);
+CREATE INDEX idx_endereco_bairro_rua ON endereco (bairro, rua);
+
+
+CREATE TABLE usuario_endereco (
+	id_usuario INT NOT NULL,
+	id_endereco INT NOT NULL,
+	
+	FOREIGN KEY (id_usuario) REFERENCES usuario (id)
+		ON DELETE NO ACTION
+		ON UPDATE CASCADE,
+		
+	FOREIGN KEY (id_endereco) REFERENCES endereco (id)
+		ON DELETE NO ACTION
+		ON UPDATE CASCADE,
+		
+	PRIMARY KEY (id_usuario, id_endereco)
+);
+CREATE UNIQUE INDEX unq_usuario_endereco_usuario ON usuario_endereco (id_usuario);
+CREATE UNIQUE INDEX unq_usuario_endereco_endereco ON usuario_endereco (id_endereco);
